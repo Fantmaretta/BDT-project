61cb2f294b99742e2ea8df983626e942110af74f
vorsanigo <verorsanigo@gmail.com>
2021-04-24 19:32:32 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,20 @@
+import datetime
+
+class Data:
+    def __init__(self, data: datetime, tmin: float, tmax: float, rain: float, temperatura: float, precipitazioni: float, venti: dict, radiazione: float, umidita_relativa: float)
+        self.data = data
+        self.tmin = tmin
+        self.tmax = tmax
+        self.rain = rain
+        self.temperatura = temperatura
+        self.precipitazioni = precipitazioni
+        self.venti = venti
+        self.radiazione = radiazione
+        self.umidita_relativa = umidita_relativa
+
+class Vento:
+    def __init__(self, v: float, d: float):
+        self.v = v
+        self.d = d
+
 # TODO decide hw to structure from xml to  -> ?
