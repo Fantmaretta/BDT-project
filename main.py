16461b4ee51b1d76bfc23c128e73d981ae14ff40
vorsanigo <verorsanigo@gmail.com>
2021-04-28 10:22:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 print("ciao mondooooooooooooooooooooooooooooooooooooooooooooooooooooooo")
 # ciao
+
+f= open("guru99.txt","w+")
+for i in range(10):
+     f.write("This is line %d\r\n" % (i+1))
+f.close() 
