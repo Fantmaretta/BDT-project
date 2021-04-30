85f7700b44e362349391b637c142b16929f46fcb
vorsanigo <verorsanigo@gmail.com>
2021-04-30 12:33:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-print("ciao mondooooooooooooooooooooooooooooooooooooooooooooooooooooooo")
+#print("ciao mondooooooooooooooooooooooooooooooooooooooooooooooooooooooo")
 # ciao
+import schedule
 
-f= open("guru99.txt","w+")
+
+'''f= open("guru99.txt","w+")
 for i in range(10):
      f.write("This is line %d\r\n" % (i+1))
      print("ciao ciao ciao")
-f.close()
+f.close()'''
+
+f = open("guru99.txt", "w+")
+
+def prova(file):
+     file.write("ciao")
+
+
+
+schedule.every(5).seconds.do(prova, f)
+#schedule.every().hour.do(self.fetch_prediction, url_pred)
+#schedule.every().day.do(self.fetch_prediction, url_pred)
+
