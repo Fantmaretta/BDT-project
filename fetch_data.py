e009303930014fac7478ee23a7d4dc1c4c55710e
vorsanigo <verorsanigo@gmail.com>
2021-05-01 14:52:41 +0200

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -5,7 +5,24 @@ import schedule
 import time
 import xml
 import xml.etree.cElementTree as ET
-from datetime import datetime
+from datetime import datetime, timedelta
+
+
+
+def create_times_day():
+    spacing = 15    # in minutes
+    lst = [str(i * timedelta(minutes=spacing)) for i in range(24 * 60 // spacing)]
+    print(lst)
+    return lst
+
+list_time = create_times_day()
+
+'''file = open("/home/veror/PycharmProjects/BDT project/pickle/file_code1.pickle",'rb')
+list_station_code = pickle.load(file)
+file1 = open("/home/veror/PycharmProjects/BDT project/pickle/file_name1.pickle",'rb')
+list_station_name = pickle.load(file1)'''
+file2 = open("/home/veror/PycharmProjects/BDT project/pickle/file_zip_code_name1.pickle",'rb')
+list_station_codes_names = pickle.load(file2)
 
 class Fetch:
 
@@ -81,17 +98,17 @@ class Fetch:
 
 
 
-    def fetch_single_station(self, url_data: str, station_code: str):
+    def fetch_single_station(self, url_data: str, station_code_name: (str, str)):
 
-        resp_data = requests.get(url_data + station_code)
+        resp_data = requests.get(url_data + station_code_name[0])
 
         root = ET.fromstring(resp_data.content)
 
         '''for child in root.iter('*'):
             print(child.tag)'''
 
-        if root.find('.//{http://www.meteotrentino.it/}data').text is not  None:
-            data_oggi = root.find('.//{http://www.meteotrentino.it/}data').text
+        if root.find('.//{http://www.meteotrentino.it/}data').text is not None:
+            data_oggi = (datetime.strptime(root.find('.//{http://www.meteotrentino.it/}data').text, '%Y/%m/%d')).date()
         else:
             data_oggi = None
         if root.find('.//{http://www.meteotrentino.it/}tmin').text is not None:
@@ -116,9 +133,14 @@ class Fetch:
             for data_ora in temperatura_aria.findall('.//{http://www.meteotrentino.it/}data'):
                 if data_ora is not None:
                     data, ora = (datetime.fromisoformat(data_ora.text)).date(), (datetime.fromisoformat(data_ora.text)).time()
-                date_ore_t.append((data, ora))
+                    date_ore_t.append((data, ora))
+                else:
+                    date_ore_t.append(None)
             for temperatura in temperatura_aria.findall('.//{http://www.meteotrentino.it/}temperatura'):
-                temperature.append(temperatura.text)
+                if temperatura is not None:
+                    temperature.append(float(temperatura.text))
+                else:
+                    temperature.append(None)
         data_ora_temp = list(zip(date_ore_t, temperature))
         print('1', data_ora_temp)
         print(len(data_ora_temp))
@@ -127,9 +149,17 @@ class Fetch:
         piogge = []
         for precipitazione in root.findall('.//{http://www.meteotrentino.it/}precipitazione'):
             for data_ora in precipitazione.findall('.//{http://www.meteotrentino.it/}data'):
-                date_ore_p.append(data_ora.text)
+                if data_ora is not None:
+                    data, ora = (datetime.fromisoformat(data_ora.text)).date(), (
+                        datetime.fromisoformat(data_ora.text)).time()
+                    date_ore_p.append((data, ora))
+                else:
+                    date_ore_p.append(None)
             for pioggia in precipitazione.findall('.//{http://www.meteotrentino.it/}pioggia'):
-                piogge.append(pioggia.text)
+                if pioggia is not None:
+                    piogge.append(float(pioggia.text))
+                else:
+                    piogge.append(None)
         data_ora_prec = list(zip(date_ore_p, piogge))
         print('2', data_ora_prec)
         print(len(data_ora_prec))
@@ -139,11 +169,21 @@ class Fetch:
         venti_dir = []
         for vento in root.findall('.//{http://www.meteotrentino.it/}vento_al_suolo'):
             for data_ora in vento.findall('.//{http://www.meteotrentino.it/}data'):
-                date_ore_v.append(data_ora.text)
+                if data_ora is not None:
+                    data, ora = (datetime.fromisoformat(data_ora.text)).date(), (datetime.fromisoformat(data_ora.text)).time()
+                    date_ore_v.append((data, ora))
+                else:
+                    date_ore_v.append((None, None))
             for velocita in vento.findall('.//{http://www.meteotrentino.it/}v'):
-                venti_vel.append(velocita.text)
+                if velocita is not None:
+                    venti_vel.append(float(velocita.text))
+                else:
+                    venti_vel.append(None)
             for direzione in vento.findall('.//{http://www.meteotrentino.it/}d'):
-                venti_dir.append(direzione.text)
+                if direzione is not None:
+                    venti_dir.append(float(direzione.text))
+                else:
+                    venti_dir.append(None)
         data_ora_v_d = list(zip(date_ore_v, venti_vel, venti_dir))
         print('3', data_ora_v_d)
         print(len(data_ora_v_d))
@@ -151,10 +191,8 @@ class Fetch:
         print('4', list(zip(data_ora_temp, data_ora_v_d, data_ora_prec)))
         print(len(list(zip(data_ora_temp, data_ora_v_d, data_ora_prec))))
 
-        print({'data_oggi': data_oggi, 'temp_min': temp_min, 'temp_max': temp_max, 'rain': rain, 'data_ora_temp': data_ora_temp, 'data_ora_prec': data_ora_prec, 'data_ora_v_d': data_ora_v_d}
-)
+        print({'localita': station_code_name[1], 'data_oggi': data_oggi, 'temp_min': temp_min, 'temp_max': temp_max, 'rain': rain, 'data_ora_temp': data_ora_temp, 'data_ora_prec': data_ora_prec, 'data_ora_v_d': data_ora_v_d})
 
-        print(station_code)
 
         print("-----------------------------------------------------------------\n")
 
@@ -163,10 +201,10 @@ class Fetch:
         # MA OK TANTO NOI FACCIAMO UNA MEDIA
         # in alcuni posti possono non esserci delle osservazioni -> eg: borgo valsugana non ha i venti
 
-        return {'data_oggi': data_oggi, 'temp_min': temp_min, 'temp_max': temp_max, 'rain': rain, 'data_ora_temp': data_ora_temp, 'data_ora_prec': data_ora_prec, 'data_ora_v_d': data_ora_v_d}
+        return {'station_code': station_code_name[0], 'localita': station_code_name[1], 'data_oggi': data_oggi, 'temp_min': temp_min, 'temp_max': temp_max, 'rain': rain, 'data_ora_temp': data_ora_temp, 'data_ora_prec': data_ora_prec, 'data_ora_v_d': data_ora_v_d}
 
 
-    def fetch_data(self, url_data: str, list_station_code: List[str]):
+    def fetch_data(self, url_data: str, list_station_code_name: List[tuple]):
         '''
         Given an url and one list containing stations' codes, it returns the actual meteorological data from that url
         only for the stations with those codes (localities that are meteorological stations)
@@ -174,15 +212,51 @@ class Fetch:
         :param list_station_code:
         :return:
         '''
-        list_resp_data = [self.fetch_single_station(url_data, station_code) for station_code in list_station_code]
+        list_resp_data = [self.fetch_single_station(url_data, station_code_name) for station_code_name in list_station_code_name]
         #list_resp_data = [requests.get(url_data + station_code) for station_code in list_station_code]
-
-        print(list_station_code)
 
         return list_resp_data
 
+    def from_fetch_to_repr_station(self, dict_data, list_time):
+        #list_observations_single_locality = []
+        #print(dict_data)
+        records = {}
+        for time in list_time:
+            #print((dict_data['data_oggi'], time))
+            if
+            records[(dict_data['data_oggi'], time)] = [dict_data['station_code'], dict_data['localita'], dict_data['data_oggi'], time, dict_data['temp_min'], dict_data['temp_max'], dict_data['rain']]
+            for t in dict_data['data_ora_temp']:
+                data_t = t[0][0]
+                ora_t = t[0][1]
+                temp = t[1]
+                print(data_t)
+                print(ora_t)
+                records[(data_t, ora_t)].append(data_t)
+                records[(data_t, ora_t)].append(ora_t)
+                records[(data_t, ora_t)].append(temp)
 
+            for p in dict_data['data_ora_prec']:
+                data_p = p[0][0]
+                ora_p = p[0][1]
+                prec = p[1]
+                records[(data_p, ora_p)].append(data_p)
+                records[(data_p, ora_p)].append(ora_p)
+                records[(data_p, ora_p)].append(prec)
 
+            for v in dict_data['data_ora_v_d']:
+                data_v = v[0][0]
+                ora_v = v[0][1]
+                vel_v = v[1]
+                dir_v = v[2]
+                records[(data_v, ora_v)].append(data_v)
+                records[(data_v, ora_v)].append(ora_v)
+                records[(data_v, ora_v)].append(vel_v)
+                records[(data_v, ora_v)].append(dir_v)
+
+        return(records)
+
+    def from_fetch_to_repr_tot_stations(self, output_fetch_data, list_time):
+        return [self.from_fetch_to_repr_station(out, list_time) for out in output_fetch_data]
 
 
     # TODO understand how to parse xml to create records of the deta table -> probably we can create dictionary also here
@@ -218,9 +292,25 @@ fetch = Fetch()
 '''x = fetch.fetch_single_station('http://dati.meteotrentino.it/service.asmx/ultimiDatiStazione?codice=', 'T0437')
 print(x)'''
 
-file = open("/home/veror/PycharmProjects/BDT project/pickle/file_code.pickle",'rb')
-list_station_code = pickle.load(file)
-y = fetch.fetch_data(' http://dati.meteotrentino.it/service.asmx/ultimiDatiStazione?codice=', list_station_code)
+'''print(list_station_code)
+print(len(list_station_code))
+print(list_station_name)
+print(len(list_station_name))
+print(list_station_code_names)
+print(len(list_station_code_names))'''
+
+#print(list_station_code_names)
+
+#y = fetch.fetch_data(' http://dati.meteotrentino.it/service.asmx/ultimiDatiStazione?codice=', list_station_codes_names)
+#print(y)
+'''file_name = open('pickle/prova.pickle', 'wb') # no duplicates of names od stations (since we consider different zones of same station)
+pickle.dump(y, file_name)'''
+file = open("/home/veror/PycharmProjects/BDT project/pickle/prova.pickle",'rb')
+y = pickle.load(file)
+#print(y)
+y2 = fetch.from_fetch_to_repr_tot_stations(y, list_time)
+print(y2)
+
 
 #file = open("/home/veror/PycharmProjects/BDT project/pickle/file_code.pickle",'rb')
 #list_station_code = pickle.load(file)
