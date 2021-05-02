eee11810962d1418c312d8dd10625679f2f06b9f
vorsanigo <verorsanigo@gmail.com>
2021-05-02 18:25:22 +0200

    Update fetch_previsioni.py

--- fetch_previsioni.py
+++ fetch_previsioni.py
@@ -30,13 +30,16 @@ class FetchPrevisioni:
         for pred in prediction_json['previsione']: # prediction_json['previsione'] -> type = list
 
             if pred['localita'].lower() in list_stations:
+                '''print(type(pred['giorni']))
+                print(pred['giorni'])'''
                 for giorno in pred['giorni']:
+                    #print(type(pred['giorni']))
                     for fascia in giorno['fasce']:
                         el = {}
                         el['localita'] = pred['localita'].lower()
                         #print(el['localita'])
                         el['data'] = datetime.strptime(giorno['giorno'], '%Y-%m-%d').date()
-                        el['id_previsione_giorno'] = giorno['idPrevisioneGiorno']
+                        el['id_previsione_giorno'] = pred['giorni'].index(giorno)#['giorno']#['idPrevisioneGiorno']
                         el['temp_min'] = giorno['tMinGiorno'] # on all day
                         el['temp_max'] = giorno['tMaxGiorno'] # on all day
                         el['fascia'] = fascia['fasciaOre']
@@ -63,8 +66,21 @@ class FetchPrevisioni:
                 #del pred
         #print(list_predictions)
         final_predictions_list = [Previsione.from_repr(raw_pred) for raw_pred in list_predictions]
+        #for i in final_predictions_list:
+        #print(Previsione.to_repr(i))
+        return final_predictions_list
 
-        return final_predictions_list #list_predictions
+
+def get_both(prediction_json: dict, list_stations):
+    stations_both = []
+    for pred in prediction_json['previsione']:  # prediction_json['previsione'] -> type = list
+
+        if pred['localita'].lower() in list_stations:
+            stations_both.append(pred['localita'].lower())
+
+    return(stations_both)
+
+
 
 
 if __name__ == "__main__":
@@ -77,8 +93,30 @@ if __name__ == "__main__":
 
     file = open("/home/veror/PycharmProjects/BDT project/pickle/file_name1.pickle", 'rb')
     list_station_name = pickle.load(file)
-    prev_no_stations = y = fetch_previsioni.remove_not_station(pred, list_station_name)
+    prev_no_stations = fetch_previsioni.remove_not_station(pred, list_station_name)
+    #both = get_both(pred, list_station_name)
 
     #print(prev_no_stations)
-
+    '''for i in prev_no_stations:
+        print(Previsione.to_repr(i))'''
     previsioni_manager.save(prev_no_stations)
+
+
+'''zip_both = []
+    for i in stat_code:
+        if i[1] in both:
+            zip_both.append(i)
+        else:
+            print(i)
+    print(len(stat_code))
+    print(len(zip_both))
+    print(len(list_station_name))
+    print(zip_both)
+    file = open('pickle/both_zip_stations_codes.pickle',
+                'wb')  # no duplicates of names od stations (since we consider different zones of same station)
+    pickle.dump(zip_both, file)
+
+    file = open('pickle/both_zip_stations_codes.pickle',
+                'rb')  # no duplicates of names od stations (since we consider different zones of same station)
+    x = pickle.load(file)
+    print(x)'''
