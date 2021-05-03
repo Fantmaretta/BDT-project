e6c31dbe866ac7dac23e54dceb497625db6db610
vorsanigo <verorsanigo@gmail.com>
2021-05-03 14:50:15 +0200

    Update fetch_previsioni.py

--- fetch_previsioni.py
+++ fetch_previsioni.py
@@ -87,7 +87,7 @@ if __name__ == "__main__":
 
     fetch_previsioni = FetchPrevisioni()
 
-    previsioni_manager = MysqlPrevisioniManager()
+    #previsioni_manager = MysqlPrevisioniManager()
 
     pred = fetch_previsioni.fetch_prediction("https://www.meteotrentino.it/protcivtn-meteo/api/front/previsioneOpenDataLocalita?localita")
 
@@ -99,7 +99,7 @@ if __name__ == "__main__":
     #print(prev_no_stations)
     '''for i in prev_no_stations:
         print(Previsione.to_repr(i))'''
-    previsioni_manager.save(prev_no_stations)
+    #previsioni_manager.save(prev_no_stations)
 
 
 '''zip_both = []
