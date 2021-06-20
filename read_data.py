b4f9df2f00c023c660351b71d6e0203defbaf4dc
vorsanigo <verorsanigo@gmail.com>
2021-06-20 22:53:07 +0200

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -1,9 +1,10 @@
 from pyspark.sql import SparkSession
 import findspark
+import pandas as pd
 
+# code to save database content into csv file to have it to read it as dataframe pyspark
 
-findspark.init()
-
+'''findspark.init()
 
 sparkConnector = SparkSession.builder.appName("Connector_to_MySQL_BDT")\
         .config("spark.driver.extraClassPath",
@@ -13,12 +14,64 @@ sparkConnector = SparkSession.builder.appName("Connector_to_MySQL_BDT")\
         .getOrCreate()
 
 
-connect_mysql = sparkConnector.read\
+# retrieve previsioni
+
+query_previsioni = """SELECT * FROM bdt_db_mysql.previsioni """
+
+df_previsioni = sparkConnector.read\
     .format("jdbc") \
     .option("url", "jdbc:mysql://bdtmysql.cvpe8im7hapy.us-east-2.rds.amazonaws.com:3306") \
-    .option("dbtable", "bdt_db_mysql.dati_reali") \
     .option("user", "root_bdt") \
     .option("password", "bdt_mysql") \
     .option("driver", 'com.mysql.cj.jdbc.Driver') \
+    .option("query", query_previsioni)\
+    .load()
+# .option("dbtable", "bdt_db_mysql.dati_reali")
+
+df_previsioni.show()
+# save into csv
+df_previsioni.toPandas().to_csv('df_previsioni.csv')
+
+#df_previsioni.rdd.saveAsPickleFile('pickle/df_previsioni.pickle')
+
+
+# retrieve dati_reali
+
+query_dati_reali = """SELECT * FROM bdt_db_mysql.dati_reali"""
+
+df_dati_reali = sparkConnector.read\
+    .format("jdbc") \
+    .option("url", "jdbc:mysql://bdtmysql.cvpe8im7hapy.us-east-2.rds.amazonaws.com:3306") \
+    .option("user", "root_bdt") \
+    .option("password", "bdt_mysql") \
+    .option("driver", 'com.mysql.cj.jdbc.Driver') \
+    .option("query", query_dati_reali)\
     .load()
 
+df_dati_reali.show()
+# save into csv
+df_dati_reali.toPandas().to_csv('df_dati_reali.csv')'''
+
+
+
+'''# transform time column of dati_reali to get only time (not date) -> cleaning
+df_dati_reali_ok = pd.read_csv('csv files/df_dati_reali.csv')
+df_dati_reali_ok['time'] = pd.to_datetime(df_dati_reali_ok['time']).apply(lambda x: x.time())
+df_dati_reali_ok.to_csv('csv files/csv_dati_reali_ok.csv')'''
+
+
+df_dati_reali_ok = pd.read_csv('csv files/csv_dati_reali_ok.csv')
+for el in df_dati_reali_ok['time']:
+    if el >= '00:00:00' and el < '06:00:00':
+        df_dati_reali_ok['fascia'] = '00-06'
+    elif el >= '06:00:00' and el < '12:00:00':
+        df_dati_reali_ok['fascia'] = '06-12'
+    elif el >= '12:00:00' and el < '18:00:00':
+        df_dati_reali_ok['fascia'] = '12-18'
+    elif el >= '18:00:00':
+        df_dati_reali_ok['fascia'] = '18-24'
+print(df_dati_reali_ok)
+df_dati_reali_ok.to_csv('csv files/dati_reali_.csv')
+
+
+# TODO save database into dataframes -> add column fascia oraria to dati_reali, do avg for localita, data, fascia oraria
