26afb587471660a9b2f36a637aa2c939aa1f11b6
vorsanigo <verorsanigo@gmail.com>
2021-07-12 18:20:45 +0200

    refactoring

--- computation.py
+++ computation.py
@@ -220,22 +220,19 @@ def avg_by_date(df, el, giorni):
 
 if __name__ == "__main__":
 
-    #df_12 = from_csv('csv files/df_12.csv')
-    #df_345 = from_csv('csv files/df_345.csv')
 
-    df_12_acc = from_csv('csv files/accuracy_12.csv')
-    df_345_acc = from_csv('csv files/accuracy_345.csv')
-    x = df_12_acc.select('localita').distinct().show()
+    # read df
+    df_12 = from_csv('csv files/df_12.csv')
+    df_345 = from_csv('csv files/df_345.csv')
 
-
-    '''def_accuracy_range_udf = udf(def_accuracy_range, StringType())
+    # function to compute accuracies
+    def_accuracy_range_udf = udf(def_accuracy_range, StringType())
 
     df_12 = correspondance_dati_prev(df_12, df_12.prec_dati, df_12.id_prec_int, 'compare_pioggia')
     df_12 = correspondance_dati_prev(df_12, df_12.intensita_vento_dati, df_12.id_vento_val, 'compare_vento_vel')
     df_12 = correspondance_dati_prev(df_12,  df_12.direzione_vento_dati, df_12.id_vento_dir_val, 'compare_vento_dir')
     df_12 = correspondance_dati_prev_temp(df_12, df_12['max(temperatura)'], df_12['temp_max'], 'compare_temp_max')
     df_12 = correspondance_dati_prev_temp(df_12, df_12['min(temperatura)'], df_12['temp_min'], 'compare_temp_min')
-
 
     df_345 = correspondance_dati_prev(df_345, df_345.prec_dati, df_345.id_prec_int, 'compare_pioggia')
     df_345 = correspondance_dati_prev(df_345, df_345.intensita_vento_dati, df_345.id_vento_val, 'compare_vento_vel')
@@ -252,36 +249,33 @@ if __name__ == "__main__":
 
     # save df with computed accuracies
     df_12.toPandas().to_csv('csv files/accuracy_12.csv')
-    df_345.toPandas().to_csv('csv files/accuracy_345.csv')'''
-
-    elements = ['compare_pioggia', 'compare_vento_vel', 'compare_vento_dir', 'compare_temp_max', 'compare_temp_min']
-
-    for i in range(0, 5):
-        acc_prev_giorno_loc(df_12_acc, elements[i], 'localita', 'castello tesino', 'id_previsione_giorno',  1, '12',True)
+    df_345.toPandas().to_csv('csv files/accuracy_345.csv')
 
 
-    #for i in range(0, 5):
-    #    acc_prev_giorno_loc(df_345_acc, elements[i], 'localita', 'trento', 'id_previsione_giorno',  1, '345',True)
+    # compute accuracies fractions on overall days
 
-
-    '''acc_prev(df_12, 'compare_pioggia', '_12_', True)
+    # no NaN, df_12
+    acc_prev(df_12, 'compare_pioggia', '_12_', True)
     acc_prev(df_12, 'compare_vento_vel', '_12_', True)
     acc_prev(df_12, 'compare_vento_dir', '_12_', True)
     acc_prev(df_12, 'compare_temp_max', '_12_', True)
     acc_prev(df_12, 'compare_temp_min', '_12_', True)
 
+    # with Nan , df_12
     acc_prev(df_12, 'compare_pioggia', '_12_null', False)
     acc_prev(df_12, 'compare_vento_vel', '_12_null', False)
     acc_prev(df_12, 'compare_vento_dir', '_12_null', False)
     acc_prev(df_12, 'compare_temp_max', '_12_null', False)
     acc_prev(df_12, 'compare_temp_min', '_12_null', False)
 
+    # no NaN, df_345
     acc_prev(df_345, 'compare_pioggia', '_345_', True)
     acc_prev(df_345, 'compare_vento_vel', '_345_', True)
     acc_prev(df_345, 'compare_vento_dir', '_345_', True)
     acc_prev(df_345, 'compare_temp_max', '_345_', True)
     acc_prev(df_345, 'compare_temp_min', '_345_', True)
 
+    # with Nan, df_345
     acc_prev(df_345, 'compare_pioggia', '_345_null', False)
     acc_prev(df_345, 'compare_vento_vel', '_345_null', False)
     acc_prev(df_345, 'compare_vento_dir', '_345_null', False)
@@ -289,7 +283,9 @@ if __name__ == "__main__":
     acc_prev(df_345, 'compare_temp_min', '_345_null', False)
 
 
+    # compute accuracies for day type
 
+    # day 1 2
     for i in range(1, 3):
         acc_prev_giorno(df_12, 'compare_pioggia', 'id_previsione_giorno', float(i), True)
         acc_prev_giorno(df_12, 'compare_pioggia', 'id_previsione_giorno', float(i), False)
@@ -302,6 +298,7 @@ if __name__ == "__main__":
         acc_prev_giorno(df_12, 'compare_temp_min', 'id_previsione_giorno', float(i), True)
         acc_prev_giorno(df_12, 'compare_temp_min', 'id_previsione_giorno', float(i), False)
 
+    # day 3 4 5
     for i in range(3, 6):
         acc_prev_giorno(df_345, 'compare_pioggia', 'id_previsione_giorno', float(i), True)
         acc_prev_giorno(df_345, 'compare_pioggia', 'id_previsione_giorno', float(i), False)
@@ -312,14 +309,34 @@ if __name__ == "__main__":
         acc_prev_giorno(df_345, 'compare_temp_max', 'id_previsione_giorno', float(i), True)
         acc_prev_giorno(df_345, 'compare_temp_max', 'id_previsione_giorno', float(i), False)
         acc_prev_giorno(df_345, 'compare_temp_min', 'id_previsione_giorno', float(i), True)
-        acc_prev_giorno(df_345, 'compare_temp_min', 'id_previsione_giorno', float(i), False)'''
+        acc_prev_giorno(df_345, 'compare_temp_min', 'id_previsione_giorno', float(i), False)
 
-    # compute statitstics for plot with dates
-    '''elements = ['compare_pioggia', 'compare_vento_vel', 'compare_vento_dir', 'compare_temp_max', 'compare_temp_min']
+
+    # compute statitstics for plot with dates may-july
+
+    # df_12
+    elements = ['compare_pioggia', 'compare_vento_vel', 'compare_vento_dir', 'compare_temp_max', 'compare_temp_min']
     for i in range(0, 5):
         avg_by_date(df_12, elements[i], '12')
 
+    # df_345
     for i in range(0, 5):
-        avg_by_date(df_345, elements[i], '345')'''
+        avg_by_date(df_345, elements[i], '345')
 
-#DONE UNTIL CELLECTION OF 3 LU
+
+
+    # code to compute on single stations -> for web
+
+    df_12_acc = from_csv('csv files/accuracy_12.csv')
+    df_345_acc = from_csv('csv files/accuracy_345.csv')
+    x = df_12_acc.select('localita').distinct().show()
+
+    elements = ['compare_pioggia', 'compare_vento_vel', 'compare_vento_dir', 'compare_temp_max', 'compare_temp_min']
+
+    for i in range(0, 5):
+        acc_prev_giorno_loc(df_12_acc, elements[i], 'localita', 'castello tesino', 'id_previsione_giorno', 1, '12',
+                            True)
+
+    for i in range(0, 5):
+        acc_prev_giorno_loc(df_345_acc, elements[i], 'localita', 'trento', 'id_previsione_giorno',  1, '345', True)
+
